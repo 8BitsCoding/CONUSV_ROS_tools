3140aa07f35a68f5f5dc57b313a4b7422597fdd2
kimtaehyung <taehyeong77@naver.com>
2018-07-08 15:13:55 +0900

    Indigo ROS Installation
    Indigo ROS Installation

---
+++ install_ros_indigo.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# Apache License 2.0
+# Copyright (c) 2018. taehyung kim, Hanwha Systems
+
+echo ""
+echo "[Note] Target OS version >>> Ubuntu 14.04.x"
+echo "[Note] Target ROS version >>> ROS Indigo
+echo "[Note] Catkin workspace >>> $HOME/catkin_ws
+echo ""
+echo "PRESS [ENTER] TO CONTINUE THE INSTALLATION"
+echo "IF YOU WAN TO CANCEL, PRESS [CTRL] + [C]"
+read
+
+# 환경변수 선언
+echo "[Set the target OS, ROS version and name of catkin workspace]"
+name_os_version=${name_os_version:="trusty"}
+#16.04=xenial
+#14.04=trusty
+name_ros_version=${name_ros_version:="indigo"}
+name_catkin_workspace=${name_catkin_workspace:="catkin_ws"}
+
+# apt 최신화
+echo "[Update the package lists and upgrad them]"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# 시간동기
+echo "[Install build enviroment, the chrony, ntpdate and set the ntpdate]"
+sudo apt-get install -y chrony ntpdate build-essential
+sudo ntpdate ntp.ubuntu.com
+
+# ROS 다운로드 준비
+echo "[Add the ROS repository]"
+if [ ! -e /etc/apt/sources.list.d/ros-latest.list ]; then
+  sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu ${name_os_version} main\" > /etc/apt/sources.list.d/ros-latest.list"
+fi
+
+echo "[Download the ROS keys]"
+roskey=`apt-key list | grep "ROS Builder"`
+if [ -z "$roskey" ]; then
+  sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+fi
+
+echo "[Check the ROS keys]"
+roskey=`apt-key list | grep "ROS Builder"`
+if [ -n "$roskey" ]; then
+  echo "[ROS key exists in the list]"
+else
+  echo "[Failed to receive the ROS key, aborts the installation]"
+  exit 0
+fi
+
+echo "[Update the package lists and upgrade them]"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+echo "[Install the ros-desktop-full and all rqt plugins]"
+sudo apt-get install -y ros-$name_ros_version-desktop-full ros-$name_ros_version-rqt-*
+
+echo "[Initialize rosdep]"
+sudo sh -c "rosdep init"
+rosdep update
+
+echo "[Environment setup and getting rosinstall]"
+source /opt/ros/$name_ros_version/setup.sh
+sudo apt-get install -y python-rosinstall
+
+echo "[Make the catkin workspace and test the catkin_make]"
+mkdir -p $HOME/$name_catkin_workspace/src
+cd $HOME/$name_catkin_workspace/src
+catkin_init_workspace
+cd $HOME/$name_catkin_workspace
+catkin_make
+
+echo "[Set the ROS evironment]"
+sh -c "echo \"alias eb='nano ~/.bashrc'\" >> ~/.bashrc"
+sh -c "echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc"
+sh -c "echo \"alias gs='git status'\" >> ~/.bashrc"
+sh -c "echo \"alias gp='git pull'\" >> ~/.bashrc"
+sh -c "echo \"alias cw='cd ~/$name_catkin_workspace'\" >> ~/.bashrc"
+sh -c "echo \"alias cs='cd ~/$name_catkin_workspace/src'\" >> ~/.bashrc"
+sh -c "echo \"alias cm='cd ~/$name_catkin_workspace && catkin_make'\" >> ~/.bashrc"
+
+sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
+sh -c "echo \"source ~/$name_catkin_workspace/devel/setup.bash\" >> ~/.bashrc"
+
+sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc"
+sh -c "echo \"export ROS_HOSTNAME=localhost\" >> ~/.bashrc"
+
+source $HOME/.bashrc
+
+echo "[Complete!!!]"
+exit 0
